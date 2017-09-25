4c916b5034ce6ac22985b10abe03440cfa6e58ff
agome27 <agome27@Anamago.local>
2017-09-25 10:01:08 -0500

    Agregar consumo de servico de CRM

---
+++ src/app/services/customer.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { BaseService } from './base.service';
+import { Http } from '@angular/http';
+
+@Injectable()
+export class CustomerService extends BaseService{
+
+  constructor(private http: Http) {
+    super();
+  }
+
+  public userCRM(cedula)
+  {
+    return this.http.get(`${this.baseUrl}/customer/consulta/C/${cedula}`, { headers: this.headers })
+    .map(
+    response => response.json()
+    ).catch(this.handleError);
+  }
+
+}
