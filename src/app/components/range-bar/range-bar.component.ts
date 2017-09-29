886c1484f299e03a2838d2c8b3c6034db2dd464f
hbedoya <hbedoya@bancodebogota.com.co>
2017-09-29 09:03:49 -0500

    Implementación cambios range bar en simulador

--- src/app/components/range-bar/range-bar.component.ts
+++ src/app/components/range-bar/range-bar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { async } from '@angular/core/testing';
 
 @Component({
   selector: 'bdb-range-bar',
@@ -7,17 +8,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RangeBarComponent implements OnInit {
 
-  max = 10000000;
-  actualValue;
-  range; 
+  @Input('max') max = 10000000;
+  @Input('min') min = 500000;
+  @Input('step') step = 100000;
+  @Output('valueChange') valueChange = new EventEmitter();
+  @Input('actualValue') actualValue = 0;
+  @Input('startValue') startValue = 5000000;
+  range;
+  firstTime = false;
 
   constructor() { }
 
   ngOnInit() {
+    this.isFisrtTime();
+    this.setFirstTime();
   }
 
-  getRangeValue(){
-    this.range = (this.actualValue*100/this.max)+'%';
+  notifyChange(event) {
+    this.valueChange.emit(event);
+  }
+
+  getRangeValue() {
+
+
+    let porciones = (this.max / this.step) - 1;
+    let porcionActual = (this.actualValue / this.step) - 1;
+
+    this.range = (porcionActual * (100) / porciones) + '%';
     return this.range;
+  }
+
+  setFirstTime() {
+    setTimeout(() => {
+      this.firstTime = false;
+    }, 1000);
+  }
+
+  sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  isFisrtTime() {
+    setTimeout(async () => {
+      this.firstTime = true;
+      for (let i = 0; i <= this.startValue; i += this.step) {
+        if (this.max < 1000) {
+          await this.sleep(45);
+        } else {
+          await this.sleep(5);
+        }
+        this.actualValue = i;
+      }
+    }, 10);
   }
 }
