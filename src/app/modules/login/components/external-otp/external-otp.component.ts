7300ef0e542c0517baaff0ea7eeb52c01b42c8e6
hbedoya <hbedoya@bancodebogota.com.co>
2017-10-04 18:23:04 -0500

    implementación timer

--- src/app/modules/login/components/external-otp/external-otp.component.ts
+++ src/app/modules/login/components/external-otp/external-otp.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExternalOtpComponent implements OnInit {
 
+  actualTime = 60;
+  time = 60;
+  color = '#6fdb6f';
+  strokeDashoffset = 0;
   constructor() { }
 
   ngOnInit() {
+    setTimeout(
+      async ()=>{
+        await this.sleep(1000);
+        for(let i = 1; i <= this.time ; i++){
+          this.strokeDashoffset = i*440/this.time;
+          await this.sleep(1000);
+          this.actualTime--;
+          if(this.actualTime == 50){
+            this.color = '#b90808';
+          }
+        }
+      },0
+    );
   }
+
+  sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
 
 }
