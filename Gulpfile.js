5f04d83cc394c6a748fb41db03b6401657af2d3b
lcaice3 <lcaice3@bancodebogota.com.co>
2017-10-09 07:43:39 -0500

    5prueba

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,7 @@
 //gulp 
 
 var gulp = require('gulp');
-
+var runSequence = require('run-sequence');
 
 // plugins
 
@@ -17,4 +17,21 @@ gulp.task('clean', function(cb) {
     rimraf('./dist/**/*', cb);
   });
 
- 
+  gulp.task('connectDist', function () {
+    connect.server({
+      root: 'dist/',
+      port: 9000
+    });
+  });
+  gulp.task('watch',function(){
+    gulp.watch('./app/**/*', ['clean']);
+  });
+  
+  gulp.task('serve', function() {
+    runSequence(['watch', 'connectDist']);
+  });
+  gulp.task('deploy', function() {
+    gulp.src('./dist/**').pipe(s3(s3Credentials));
+  });
+  // default task
+gulp.task('default', ['serve']);
